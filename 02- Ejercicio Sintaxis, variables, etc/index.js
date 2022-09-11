f010e85fbf29d1948e24377565090a14e720586e
Gian Franco Crova <103906625+gfCrova@users.noreply.github.com>
2022-09-11 14:58:58 -0300

    Add files via upload

---
+++ 02- Ejercicio Sintaxis, variables, etc/index.js
@@ -0,0 +1,28 @@
+
+
+var nombre = "Gian Franco Crova";
+
+console.log(nombre);
+
+var edad = 27;
+
+console.log(edad);
+
+var desarrollador = true;
+
+console.log(desarrollador);
+
+
+const fechaDeNacimiento = new Date(1995, 0, 12);
+
+console.log(fechaDeNacimiento);
+
+
+const libroFavorito = {
+    titulo: "Steve Jobs",
+    autor: "Isaacson Walter",
+    fecha: 2011,
+    url : "https://es.wikipedia.org/wiki/Steve_Jobs_(libro)"
+}
+
+console.log(libroFavorito);
