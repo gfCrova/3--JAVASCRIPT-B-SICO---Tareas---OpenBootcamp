c28cc0903f27168dcd4ceea0e8620dae5aff734d
Gian Franco Crova <103906625+gfCrova@users.noreply.github.com>
2022-10-11 23:59:31 -0300

    Add files via upload

---
+++ 13- Ejercicio ESLint/index.js
@@ -0,0 +1,33 @@
+
+// Nombre, Apellido
+
+let nombre = "Gian Franco";
+let apellido = "Crova";
+
+// Concatenar strings
+
+let estudiante = `Mi nombre es ${nombre} y mi apellido es ${apellido}, y soy estudiante en OpenBootcamp!`;
+
+// Strings en Mayúscula y en Minúscula
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+// Longitud del string estudiante
+
+let longEstudiante = estudiante.length;
+
+// Primera y Última letra de las variables "nombre" y "apellido"
+
+let primeraLetrNom = nombre[0];
+let ultimaLetraAp = apellido[4];
+
+// Eliminar espacios de la variable "estudiante"
+
+let borrarEsp = estudiante.trim();
+
+// Saber si se encuentra la var "nombre" en el string de la var "estudiante"
+
+let buscarNom = estudiante.includes(nombre);
+
+
